9263041b248356de3062a72c110bc7cda88efc02
Steven Jimenez <stevenjimenez2029@gmail.com>
2021-06-01 15:30:12 -0400

    created components, services and initialized firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzdiRo9Lizo5E1qPs58U_5rucvd6iYqlk",
+    authDomain: "wechat-c1a06.firebaseapp.com",
+    projectId: "wechat-c1a06",
+    storageBucket: "wechat-c1a06.appspot.com",
+    messagingSenderId: "1992554008",
+    appId: "1:1992554008:web:ed19eff0fd0c5046f7f56d"
+  }
 };
