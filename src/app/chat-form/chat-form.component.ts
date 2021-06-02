e125b957b9286b1f2beec79b1b2e8a51ed58ea8b
Steven Jimenez <stevenjimenez2029@gmail.com>
2021-06-02 15:25:40 -0400

    Mocked up chat service

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat-form',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-form.component.css']
 })
 export class ChatFormComponent implements OnInit {
+  message!: string;
 
-  constructor() { }
+  constructor(private chat: ChatService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  send() {
+    this.chat.sendMessage(this.message);
+    this.message = '';
+  }
+
+  handleSubmit(event: { keyCode: number; }) {
+    if (event.keyCode === 13) {
+      this.send();
+    }
+  }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,65 @@
+
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase/app';
+import { ChatMessage } from '../models/chat-message.model';
+// import { timeStamp } from 'console';
+import { firestore } from 'firebase/app';
+import Timestamp = firestore.Timestamp;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  user: any;
+  chatMessages!: AngularFireList<any>;
+  chatMessage!: ChatMessage;
+  userName!: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+    ) {
+    //     this.afAuth.authState.subscribe(auth => {
+    //       if (auth !== undefined && auth !== null) {
+    //         this.user = auth;
+    //       }
+    // });
+  }
+
+  sendMessage(msg: string) {
+    const timestamp = this.getTimeStamp();
+    // const email = this.user.email;
+    const email = "text@example.com";
+    this.chatMessages = this.getMessages();
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      // userName: this.userName,
+      userName: "test-user",
+      email: email });
+
+      console.log('Called sendMessage');
+  }
+
+  getMessages(): AngularFireList<ChatMessage[]> {
+    return this.db.list('/messages', ref => {
+      return ref.limitToLast(25).orderByKey();
+    });
+  }
+
+  getTimeStamp() {
+    const now = new Date();
+    const date = now.getUTCFullYear() + '/' +
+                 (now.getUTCMonth() + 1) + '/' +
+                 now.getUTCDate();
+    const time = now.getUTCHours() + ':' +
+                 now.getUTCMinutes() + ':' +
+                 now.getUTCSeconds();
+
+    return (date + ' ' + time);
+  }
 }
